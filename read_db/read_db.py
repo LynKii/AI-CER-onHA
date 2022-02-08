e7d1b504b3be72e6ef2ec1daad3428f1eeaa4108
LynKii <LynKii@iCloud.com>
2022-02-09 05:29:31 +0800

    修改：读取天气

--- read_db/read_db.py
+++ read_db/read_db.py
@@ -1,4 +1,14 @@
-import sqlite3 as sql
+# coding=UTF-8
+import sqlite3
 
 # 创建连接对象
-db = sql.connect('home-assistant_v2.db')
+conn = sqlite3.connect('home-assistant_v2.db')
+
+# 获取天气状况
+def weatherState():
+    with conn:
+        weather_state = conn.execute("select state from states where domain = 'weather' order by state_id desc limit 0, 1")
+        for i in weather_state:
+            print(i)
+
+weatherState()
