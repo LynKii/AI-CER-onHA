1ad46f16b974cf5c951a9abf8a04edc7259ab0a6
LynKii <LynKii@iCloud.com>
2022-03-16 16:15:42 +0800

    新增：资讯页面框架，登录页面

--- AI-CER-frontend/pages/user/user.js
+++ AI-CER-frontend/pages/user/user.js
@@ -1,24 +1,60 @@
-// pages/user/user.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
+    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
 
-  // 事件处理函数
-  bindViewTap() {
+  onLoad() {
+  //  装事件
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo:app.globalData.userInfo,
+        hasUserInfo:true
+      })
+   } else if(this.data.canIUse){
+    //  由于getUserInfo 是网络请求，可能会在Page.onload 之后才返回
+    // 所以此处加入 callback 以防止这种情况
+    app.userInfoReadyCallback = res =>{
+      this.setData({
+        userInfo:res.userInfo,
+        hasUserInfo:true
+      })
+    }
+   } else {
+    //  在没有 open-type-getUserInfo 版本的兼容处理
+    wx.getUserInfo({
+      success: res =>{
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo:res.userInfo,
+          hasUserInfo:true
+        })
+      }
+    })
+   }
+   if(this.data.hasUserInfo){
+     wx.navigateTo({
+       url: './home',
+     })
+     app.globalData.userInfo = e.detail.userInfo
+     this.setData({
+       userInfo:e.detail.userInfo,
+       hasUserInfo:true
+     })
+   }
+  },
+
+  getUserInfo:function(e){
+    console.log(e)
     wx.navigateTo({
-      url: '../logs/logs'
+      url: './home',
     })
   },
-
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -32,64 +68,13 @@ Page({
       }
     })
   },
+  getUserInfo(e) {
+    // 直接返回匿名的用户个人信息
+    console.log(e)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
